3eef139648929f47b71cc8e79331eb3190c4e50c
Fredrik Nymoen <fredriknymoen2004@gmail.com>
2025-10-26 01:48:35 +0200

    change git author

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-red-500 text-3xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
